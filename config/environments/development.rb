bb559ae127c5ede74685250b70a916ff115518e8
Marcelo Mota <mmarcelomota123@gmail.com>
2024-11-21 12:55:20 -0400

    factora:primeiro commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -73,4 +74,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # config.assets.prefix = "/dev-assets"
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,24 +1,36 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-    './node_modules/flowbite/**/*.js' // Adicione esta linha para incluir o Flowbite
+    './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    colors: {
+      ...colors,
+      primary: {
+        DEFAULT: '#FF5A5F',
+        dark: '#FF385C',
+      }
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
       },
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-    require('flowbite/plugin') // Adicione o plugin do Flowbite
-  ],
+  ]
 }
